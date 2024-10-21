a25b3b68ede47c97d16d9314cf12dfa880f17404
Daria Kuliashova <daria.kuliashova@scootapi.com>
2024-10-21 13:59:49 +0200

    add env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -10,37 +11,42 @@ app.use(bodyParser.json());
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "daria.kuliashova@gmail.com",
-    pass: "12431243DashsaLeshsa", // Замените на пароль от почты
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
 app.post("/send-email", (req, res) => {
   const { firstname, lastname, email, phone, message } = req.body;
 
+  if (!firstname || !lastname || !email || !message) {
+    return res.status(400).send("All fields are required.");
+  }
+
   const mailOptions = {
     from: email,
-    to: "daria.kuliashova@gmail.com",
+    to: process.env.EMAIL_TO,
     subject: "New Contact Form Submission",
     text: `
       Name: ${firstname} ${lastname}
       Email: ${email}
-      Phone: ${phone}
+      Phone: ${phone || "N/A"}
       Message: ${message}
     `,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
+      console.error(error);
       res.status(500).send("Error sending email");
     } else {
       console.log("Email sent: " + info.response);
-      res.status(200).send("Email sent");
+      res.status(200).send("Email sent successfully");
     }
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
